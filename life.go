4c38d8c01a539aaa0691c4e521b2182a07d9cee4
Mike Santo <mike@actionsoftware.co>
2017-11-11 22:09:20 -0600

    init matrix

--- life.go
+++ life.go
@@ -1,13 +1,30 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "math/rand"
+  // "time"
+)
 
 func main() {
-  messages := make(chan string)
-  
-  go func() { messages <- "ping" }()
-  
-  msg := <- messages
-  
-  fmt.Println(msg)
+  // messages := make(chan string)
+  // go func() { messages <- "ping" }()
+  // msg := <- messages
+  // fmt.Println(msg)
+  rows, columns := 256, 256
+  matrix := initMatrix(rows, columns)
+  fmt.Println(matrix)
+}
+
+func initMatrix(rows int, columns int) [][]bool {
+  matrix := make([][]bool, rows)
+
+  for i := 0; i < rows; i++ {
+    matrix[i] = make([]bool, columns)
+    for j := 0; j < columns; j++ {
+      matrix[i][j] = rand.Intn(2) == 0
+    }
+  }
+
+  return matrix
 }
